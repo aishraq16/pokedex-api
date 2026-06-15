df7a7844eafe2f66f0dfa31a475d645191173997
aishraq16 <ishraqafzal16@gmail.com>
2026-06-15 08:56:57 +0800

    created the router

---
+++ app.js
@@ -0,0 +1,7 @@
+// phase 1
+//create an express server listening on port 3000
+//make a route like /pokemon/:name so when users visit /pokemon/pikachu, 
+// they will see the details of pikachu
+//if a user visits a route with query parameters, retrieve the parameters
+//and send back a custom message
+
---
+++ routes/pokedexRouter.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const pokedexRouter = Router();
+
+pokedexRouter.get('/:name', (req, res) => {
+    const { name } = req.params;
+    // Here you would typically fetch the details of the Pokemon from a database or an API
+    // For demonstration purposes, we'll just send back a custom message
+    res.send(`You requested details for ${name}`);
+});
+
+pokedexRouter.get('/', (req, res) => {
+    res.send("You requested for the list of all Pokemon");
+});
+
+module.exports = pokedexRouter;
